c3f68ad81ea61348b49e462b4ae1b49fc6926b28
tariqsyed1 <trexrh@gmail.com>
2020-08-10 20:25:14 -0400

    first commit

---
+++ secrets.py
@@ -0,0 +1,3 @@
+# Make sure to fill in your spotify client_secret information
+spotify_token = "BQAZWcnC-qBZ_7qmJvlp2ehgo3OW45-ZUrKBpoDzW-lbTjQe9XmUzgKLzn3Dnu-6nOGv_De3aox5LU5l0VbaRqRz4tWyqUwNLqx_MjLL76FB22WzcWmlgwNcBiFZ9qAD3Lr622twHWCHHAakPolo5qI"
+spotify_user_id = "tariqsyed1"
