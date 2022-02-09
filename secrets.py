e538579f5db179228f238a832f9df77d66e9a336
Tariq Syed <trexrh@gmail.com>
2022-02-09 14:52:32 -0500

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,3 +1,3 @@
 
-spotify_token = "BQAZWcnC-qBZ_7qmJvlp2ehgo3OW45-ZUrKBpoDzW-lbTjQe9XmUzgKLzn3Dnu-6nOGv_De3aox5LU5l0VbaRqRz4tWyqUwNLqx_MjLL76FB22WzcWmlgwNcBiFZ9qAD3Lr622twHWCHHAakPolo5qI"
+spotify_token = ""
 spotify_user_id = "tariqsyed1"
